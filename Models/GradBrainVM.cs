411cee31cadbc355434aeb1f6254abc91adc63ca
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-03-18 12:14:20 +0300

    General fixes

--- Models/GradBrainVM.cs
+++ Models/GradBrainVM.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Media;
+using System;
 using System.Windows.Media.Imaging;
+using Photozhop.Utility;
+
 
 namespace Photozhop.Models
 {
@@ -20,6 +17,7 @@ namespace Photozhop.Models
 
 		private byte[] _bytes;
 		private PointF[] InterpolatedPoints;
+		private InterpolatedInterval[] intervals;
 		public ObservableCollection<PointF> points;
 		public BitmapSource Image
 		{
@@ -50,7 +48,6 @@ namespace Photozhop.Models
 			this.Bytes = src.Bytes;
 			points = new ObservableCollection<PointF>();
 			points.Add(new PointF(0, 0));
-			InterpolatePoints();
 			points.CollectionChanged += (s, e) => InterpolatePoints(); // Возможно надо убрать
 		}
 
@@ -64,44 +61,86 @@ namespace Photozhop.Models
 			float[] Fs = new float[Hs.Length - 1];
 			for (int i = 0; i < Hs.Length; i++)         // Maybe Parallel?
 				Hs[i] = lstPoints[i + 1].X - lstPoints[i].X;
-			for (int i = 1; i < Hs.Length - 1; i++)     // Maybe Parallel?
+			for (int i = 1; i < Hs.Length; i++)     // Maybe Parallel?
 			{
 				As[i - 1, 0] = Hs[i];
-				As[i - 1, 1] = 2 * (Hs[i] + Hs[i + 1]);
-				As[i - 1, 2] = Hs[i + 1];
+				As[i - 1, 1] = 2 * (Hs[i-1] + Hs[i]);
+				As[i - 1, 2] = Hs[i];
 				float y0 = lstPoints[i - 1].Y;
 				float y1 = lstPoints[i].Y;
 				float y2 = lstPoints[i + 1].Y;
-				Fs[i] = 6 * (((y2 - y1) / Hs[i + 1]) - ((y1 - y0) / Hs[i]));
+				Fs[i-1] = 6 * (((y2 - y1) / Hs[i]) - ((y1 - y0) / Hs[i-1]));
 			}
 
 			float[] alpha = new float[Hs.Length];
 			float[] beta = new float[Hs.Length];
 			alpha[0] = As[0, 1]; beta[0] = Fs[0];
-			for (int i = 1; i < alpha.Length; i++)
+			if (Hs.Length > 2)
 			{
-				alpha[i] = (As[i, 1] * As[i - 1, 2]) / alpha[i - 1];
-				beta[i] = Fs[i] - beta[i] * (As[i, 0] / alpha[i - 1]);
+				for (int i = 1; i < alpha.Length; i++)
+				{
+					alpha[i] = (As[i, 1] * As[i - 1, 2]) / alpha[i - 1];
+					beta[i] = Fs[i] - beta[i] * (As[i, 0] / alpha[i - 1]);
+				}
 			}
 
 			float[] c = new float[lstPoints.Count - 1];
 			float[] a = (from p in lstPoints
-						select p.Y).ToArray();
+						 where p != lstPoints.First()
+						 select p.Y).ToArray();
 			float[] b = new float[lstPoints.Count - 1];
 			float[] d = new float[lstPoints.Count - 1];
-			c[c.Length] = beta.Last() / alpha.Last();
-			for (int i = c.Length - 1; i > -1; i--)
+			c[c.Length-1] = beta.Last() / alpha.Last();
+			for (int i = c.Length - 2; i > -1; i--)
 				c[i] = (beta[i] - As[i, 2] * c[i + 1]) / alpha[i];
+			for (int i = 1; i < b.Length; i++)
+			{
+				b[i] = (((lstPoints[i].Y - lstPoints[i - 1].Y) / Hs[i])) + (c[i] * Hs[i] / 3f) + (c[i - 1] * Hs[i] / 6);
+				d[i] = (c[i] - c[i - 1]) / Hs[i];
+			}
+			intervals = (from i in Enumerable.Range(0, a.Length)
+						 select new InterpolatedInterval
+						 {
+							 A = a[i],
+							 B = b[i],
+							 C = c[i],
+							 D = d[i],
+							 start = lstPoints[i].X,
+							 end = lstPoints[i + 1].X
+						 }).ToArray();
+			//intervals = (from aa in a
+			//			 from bb in b
+			//			 from cc in c
+			//			 from dd in d
+			//			 select new InterpolatedInterval
+			//			 {
+			//				 A = aa, B = bb, C = cc, D = dd
+			//			 }).ToArray();
+			//for (int i = 0; i < intervals.Length-1; i++)
+			//{
+			//	intervals[i].start = lstPoints[i].X;
+			//	intervals[i].end = lstPoints[i + 1].X;
+			//}
+			uint j = 0;
+			InterpolatedPoints = new PointF[100];
+			for (int i = 0; i < 100; i++)
+			{
+				if (intervals[j].InRange(i / 99))
+				{
+					float tmpY = intervals[j].A + intervals[j].B * (i / 99 - intervals[j].start) + intervals[j].C * 0.5f * (i / 99 - intervals[j].start) * (i / 99 - intervals[j].start) + (intervals[j].D / 6f) * (i / 99 - intervals[j].start) * (i / 99 - intervals[j].start) * (i / 99 - intervals[j].start);
+					InterpolatedPoints[i] = new PointF(i / 99, tmpY);
+				}
+				else j++;
 
-
+			}
 		}
 
-		public Point[] GetPoints(int k)
+		public Point[] GetPoints(int k,bool invertY)
 		{
 			Point[] ps = new Point[InterpolatedPoints.Length];
-			for (int i = 0; i < InterpolatedPoints.Length; i++)
+			for (int i = 0; i < ps.Length; i++)
 			{
-				ps[i] = new Point((int)(InterpolatedPoints[i].X * k), (int)(InterpolatedPoints[i].Y * k));
+				ps[i] = new Point((int)InterpolatedPoints[i].X*k, invertY? (1-(int)InterpolatedPoints[i].Y) * k : (int)InterpolatedPoints[i].Y * k);
 			}
 			return ps;
 		}
---
+++ Utility/InterpolatedInterval.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photozhop.Utility
+{
+    struct InterpolatedInterval
+    {
+        public float A;
+        public float B;
+        public float C;
+        public float D;
+        public float start;
+        public float end;
+        public bool InRange(float x) => x >= start && x <= end;
+        
+    }
+}
