4ff8e36c742656189fc538a2eb985a47706794ec
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-04-12 19:58:01 +0300

    Добавлен выбор фильтров и улучшена обработка изображений
    В файле `MatrixWindow.xaml` добавлен элемент `ComboBox` для выбора фильтров, изменены размеры `DataGrid` и `Button`. В `MatrixTransformVM.cs` размер массива `_array` изменен с 3x3 на 5x5, добавлено свойство `Filter` и массив `Filters`. Логика применения фильтров обновлена: теперь используется метод `MatrixTransform` с параллельной обработкой для повышения производительности.

--- Models/MatrixTransformVM.cs
+++ Models/MatrixTransformVM.cs
@@ -13,10 +13,12 @@ namespace Photozhop.Models
 	{
 		private ImageModel src;
 		private BitmapSource _image;
-		private float[,] _array = new float[3, 3];
+		private float[,] _array = new float[5, 5];
 		private byte[] data;
 		private ICommand applyFilter;
+		private string _filter;
 
+		public string[] Filters => new string[] { "Matrix Transform", "Median Blur" };
 		public float[,] Array
 		{
 			get => _array;
@@ -24,6 +26,16 @@ namespace Photozhop.Models
 			{
 				_array = value;
 				OnPropertyChanged(nameof(Array));
+			}
+		}
+
+		public string Filter
+		{
+			get => _filter;
+			set
+			{
+				_filter = value;
+				OnPropertyChanged(nameof(Filter));
 			}
 		}
 
@@ -49,44 +61,63 @@ namespace Photozhop.Models
 			{
 				return applyFilter ??= new RelayCommand((_) => true, (_) =>
 				{
-					int size = src.Width * src.Height;
-					data = new byte[src.Bytes.Length];
-					src.Bytes.CopyTo(data, 0);
-					for (int i = 0; i < size; i++)
+					switch (Filter)
 					{
-						int y = i / src.Width;
-						int x = i - y * src.Width;
-						int up = y - 3 / 2, down = y + 3 / 2;
-						int left = x - 3 / 2, right = x + 3 / 2;
-						float bSum = 0f, gSum = 0f, rSum = 0f;
-						for (int a = up; a <= down; a++)
-						{
-							int Y = a;
-							if (Y < 0) Y = Y + 3 - 1 - (a - up) * 2;
-							else if (Y >= src.Height) Y = Y - 3 + 1 + (down - a) * 2;
-							for (int b = left; b <= right; b++)
-							{
-								int X = b;
-								if (X < 0) X = X + 3 - 1 - (b - left) * 2;
-								else if (X >= src.Width) X = X - 3 + 1 + (right - b) * 2;
-								bSum += data[Y * src.Width * 4 + X * 4] * _array[a - up, b - left];
-								gSum += data[Y * src.Width * 4 + X * 4 + 1] * _array[a - up, b - left];
-								rSum += data[Y * src.Width * 4 + X * 4 + 2] * _array[a - up, b - left];
-
-							}
-
-						}
-						bSum = Mathematics.Clamp(bSum / 8, 0, 255);
-						gSum = Mathematics.Clamp(gSum / 8, 0, 255);
-						rSum = Mathematics.Clamp(rSum / 8, 0, 255);
-						data[i * 4] = (byte)Math.Round(bSum, 0);
-						data[i * 4 + 1] = (byte)Math.Round(gSum, 0);
-						data[i * 4 + 2] = (byte)Math.Round(rSum, 0);
+						case "Matrix Transform":
+							MatrixTransform();
+							break;
+						case "Median Blur":
+							//MedianBlur();
+							break;
+						default:
+							break;
 					}
-					Image = BitmapSource.Create(src.Width, src.Height, 96, 96, System.Windows.Media.PixelFormats.Bgra32, null, data, src.Width * 4);
-					Image.Freeze();
+
 				});
 			}
+		}
+
+		public void MatrixTransform()
+		{
+			int size = src.Width * src.Height;
+			//data = new byte[src.Bytes.Length];
+			byte[] data_copy = new byte[src.Bytes.Length];
+			src.Bytes.CopyTo(data_copy, 0);
+			//src.Bytes.CopyTo(data, 0);
+			Parallel.For(0, size, (i) =>
+			//for (int i = 0; i < size; i++)
+			{
+				int y = i / src.Width;
+				int x = i - y * src.Width;
+				int up = y - _array.GetLength(0) / 2, down = y + _array.GetLength(0) / 2;
+				int left = x - _array.GetLength(1) / 2, right = x + _array.GetLength(1) / 2;
+				float bSum = 0f, gSum = 0f, rSum = 0f;
+				for (int a = up; a <= down; a++)
+				{
+					int Y = a;
+					if (Y < 0) Y = Y + _array.GetLength(0) - 1 - (a - up) * 2;
+					else if (Y >= src.Height) Y = Y - _array.GetLength(0) + 1 + (down - a) * 2;
+					for (int b = left; b <= right; b++)
+					{
+						int X = b;
+						if (X < 0) X = X + _array.GetLength(1) - 1 - (b - left) * 2;
+						else if (X >= src.Width) X = X - _array.GetLength(0) + 1 + (right - b) * 2;
+						bSum += src.Bytes[Y * src.Width * 4 + X * 4] * _array[a - up, b - left];
+						gSum += src.Bytes[Y * src.Width * 4 + X * 4 + 1] * _array[a - up, b - left];
+						rSum += src.Bytes[Y * src.Width * 4 + X * 4 + 2] * _array[a - up, b - left];
+
+					}
+
+				}
+				bSum = Mathematics.Clamp(bSum, 0, 255);
+				gSum = Mathematics.Clamp(gSum, 0, 255);
+				rSum = Mathematics.Clamp(rSum, 0, 255);
+				data_copy[i * 4] = (byte)Math.Round(bSum, 0);
+				data_copy[i * 4 + 1] = (byte)Math.Round(gSum, 0);
+				data_copy[i * 4 + 2] = (byte)Math.Round(rSum, 0);
+
+			}); Image = BitmapSource.Create(src.Width, src.Height, 96, 96, System.Windows.Media.PixelFormats.Bgra32, null, data_copy, src.Width * 4);
+			Image.Freeze();
 		}
 	}
 }
