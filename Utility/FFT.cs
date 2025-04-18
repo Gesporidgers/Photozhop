6a6b89495e5c3894b2fbe50dc5207250f97b13a1
Gesporidgers <49988107+Gesporidgers@users.noreply.github.com>
2025-04-19 00:53:36 +0300

    Начало 5 лабы

---
+++ Utility/FFT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photozhop.Utility
+{
+    class FFT
+    {
+        public static Complex[] ditfft(Complex[] arr,int x0,int N, int s)
+        {
+            Complex[] X = new Complex[N];
+            if (N == 1)
+                X[0] = arr[x0];
+            else
+            {
+                ditfft(arr, x0, N / 2, 2 * s).CopyTo(X,0);
+                ditfft(arr, x0 + s, N / 2, 2 * s).CopyTo(X, N/2);
+
+			}
+        }
+    }
+}
